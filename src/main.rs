8301cc739debb2e0834064abf0a41d8d6d15a483
yourunclesdog <konpeito.cyou>
2023-11-08 22:48:36 +0000

    Allows you to input a song which is fed into a vec, and will use configuration details to log to last.fm

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,53 @@
 extern crate rustfm_scrobble;
 use rustfm_scrobble::{Scrobble, Scrobbler};
+use serde::Deserialize;
+use std::{fs::File, io, io::prelude::*};
+
+#[derive(Deserialize)]
+struct Config {
+    login: Login,
+}
+
+#[derive(Deserialize)]
+struct Login {
+    apikey: String,
+    apisecret: String,
+    username: String,
+    password: String,
+}
 
 fn main() {
-    let username = "";
-    let password = "";
-    let api_key = "";
-    let api_secret = "";
+    let song_info = vec_from_input();
 
-    let mut scrobbler = Scrobbler::new(api_key, api_secret);
-    let _ = scrobbler.authenticate_with_password(username, password);
+    let mut file = File::open("config.toml").unwrap();
+    let mut config = String::new();
+    _ = file.read_to_string(&mut config);
+    let config: Config = toml::from_str(&config[..]).unwrap();
 
-    let song = &Scrobble::new("black midi", "Near DT, MI", "Schlagenheim");
+    let mut scrobbler = Scrobbler::new(&config.login.apikey[..], &config.login.apisecret[..]);
+    let _ = scrobbler.authenticate_with_password(&config.login.username, &config.login.password);
+
+    let song = &Scrobble::new(&song_info[0][..], &song_info[1][..], &song_info[2][..]);
     let _ = scrobbler.scrobble(song);
+}
+
+fn vec_from_input() -> Vec<String> {
+    let stdin = io::stdin();
+
+    let mut buffer = String::new();
+    println!("input song name: ");
+    stdin.read_line(&mut buffer).unwrap();
+    let song_name = buffer;
+
+    let mut buffer = String::new();
+    println!("by: ");
+    stdin.read_line(&mut buffer).unwrap();
+    let song_artist = buffer;
+
+    let mut buffer = String::new();
+    println!("from the album: ");
+    stdin.read_line(&mut buffer).unwrap();
+    let song_album = buffer;
+
+    vec![song_artist, song_name, song_album]
 }
